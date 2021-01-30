499937897cbcdc97a82c0d0c7d65182ba4f7b6fd
Hanminseo <hm5260@naver.com>
2021-01-30 22:57:26 +0900

    Initial commit 마이갤러리 깃허브 연습용

---
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.mygallery
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import java.util.jar.Manifest
+import kotlin.concurrent.timer
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_EXTERNAL_STORAGE = 1000
+    lateinit var viewPager : ViewPager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager)
+
+        if(ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                var dlg = AlertDialog.Builder(this)
+                dlg.setTitle("권한이 필요한 이유")
+                dlg.setMessage("사진 정보를 얻기 위해서는 외부 저장소 권한이 필수로 필요합니다.")
+                dlg.setPositiveButton("확인"){dialog, which ->  ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_EXTERNAL_STORAGE)}
+
+                dlg.setNegativeButton("취소", null)
+                dlg.show()
+            }else{
+                ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_EXTERNAL_STORAGE)
+            }else{
+            getAllPhotos()
+        }
+    }
+
+    private fun getAllPhotos(){
+        //모든 사진 정보 가져오기
+        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        null, null, null, MediaStore.Images.ImageColumns.DATE_TAKEN
+        + " DESC") // 가져올 항목 배열 조건 조건 촬영 최신 날짜
+
+        val fragments = ArrayList<Fragment>()
+
+        if (cursor != null){
+            while (cursor.moveToNext()){
+                val uri = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                Log.d("MainActivity", uri)
+                fragments.add(PhotoFragment.newInstance(uri))
+            }
+            cursor.close()
+        }
+
+        val adapter = MyPagerAdapter(supportFragmentManager)
+        adapter.updateFragments(fragments)
+        viewPager.adapter = adapter
+
+        timer(period = 3000){
+            runOnUiThread(){
+                if (viewPager.currentItem < adapter.count-1){
+                    viewPager.currentItem++
+                }else{
+                    viewPager.currentItem=0                }
+            }
+        }
+
+    }
+}
